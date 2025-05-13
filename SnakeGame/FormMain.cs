f0855e4ba310a5d22d716fafa8e227cda0e9b3ee
alantsarakov <alant@DESKTOP-VS9G2NO>
2025-05-13 22:44:56 +0300

    Добавьте файлы проекта.

---
+++ SnakeGame/FormMain.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SnakeGame
+{
+    public partial class FormMain : Form
+    {
+        Snake snake;
+        public FormMain()
+        {
+            InitializeComponent();
+
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            snake = new Snake(this, 10, 10, 100);
+            
+        }
+
+        private void FormMain_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                snake.SetSnakeDirection(Snake.SnakeDirection.Up);
+                snake.MoveSnake();
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                snake.SetSnakeDirection(Snake.SnakeDirection.Down);
+                snake.MoveSnake();
+            }
+            else if (e.KeyCode == Keys.Right)
+            {
+                snake.SetSnakeDirection(Snake.SnakeDirection.Right);
+                snake.MoveSnake();
+            }
+            else if (e.KeyCode == Keys.Left)
+            {
+                snake.SetSnakeDirection(Snake.SnakeDirection.Left);
+                snake.MoveSnake();
+            }
+        }
+    }
+}
---
+++ SnakeGame/Snake.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SnakeGame
+{
+    public class Snake : PictureBox
+    {
+        private SnakeDirection snakeDirection = SnakeDirection.Up;
+        int SideSize;
+        int x, y;
+        public Snake(Form form, int sideSize, int x, int y) 
+        {
+            this.BackgroundImage = System.Drawing.Image.FromFile("Square.png");
+            this.BackgroundImageLayout = ImageLayout.Stretch;
+            SideSize = sideSize;
+            this.Width = SideSize;
+            this.Height = SideSize;
+            form.Controls.Add(this);
+            this.x = x;
+            this.y = y;
+            this.Location = new Point(x, y);
+
+
+           
+        }
+        public enum SnakeDirection
+        {
+            Left,
+            Right,
+            Up,
+            Down
+        }
+        private SnakeDirection GetSnakeDirection()
+        {
+            return snakeDirection;
+        }
+        public void SetSnakeDirection(SnakeDirection direction)
+        {
+            snakeDirection = direction;
+        }
+
+        public void MoveSnake()
+        {
+           if (snakeDirection == SnakeDirection.Down)
+            {
+                y += SideSize;
+            }
+           else if (snakeDirection == SnakeDirection.Up)
+            {
+                y -= SideSize;
+            }
+           else if (snakeDirection == SnakeDirection.Right)
+            {
+                x += SideSize;
+            }
+           else if (snakeDirection == SnakeDirection.Left)
+            {
+                x -= SideSize;
+            }
+            this.Location = new Point(x, y);
+        }
+    }
+}
